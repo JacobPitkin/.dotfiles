766eb33e731ac53227d9fa60cba3f82ab32694c4
Jacob Pitkin <jacob.pitkin@powin.com>
2024-09-09 12:57:59 -0700

    Adding starship to install script, as well as giving an echo for a default case

--- install.sh
+++ install.sh
@@ -6,17 +6,22 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	
 	case $ID in
 		arch)
+			curl -sS https://starship.rs/install.sh | sh
 			sudo pacman -S zoxide fzf bat tree
 			;;
 
 		*)
+			curl -sS https://starship.rs/install.sh | sh
 			sudo apt install zoxide fzf bat tree
 			;;
 	esac
 elif [[ "$OSTYPE" == "darwin"* ]]; then
+	curl -sS https://starship.rs/install/sh | sh
 	brew install --cask font-meslo-lg-nerd-font
 	brew install zoxide\
 	fzf\
 	bat\
 	tree
+else
+	echo "No commands given to install on this OS. Peek at the install script to get an idea of what needs installing (it's relatively short)."
 fi
